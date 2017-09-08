e35926c44aa4b2a03cf1a73395bd7c229f57cbd9
zhenqing <zhaozhenqing@gmail.com>
2017-09-08 14:37:57 -0700

    add walmart crawler

--- src/com/ibport/isr/WalmartWeb.java
+++ src/com/ibport/isr/WalmartWeb.java
@@ -1,5 +1,6 @@
 package com.ibport.isr;
 
+
 import java.io.IOException;
 
 import org.jsoup.Jsoup;
@@ -11,53 +12,55 @@ import com.ibport.model.WalmartProduct;
 
 public class WalmartWeb {
 
-	public static String URL_ROOT1 = "http://www.walmart.com/browse/video-games/playstation-3-accessories/2636_413799_1225016?page=";
-	public static String URL_ROOT2 = "&cat_id=2636_413799_1225016";
-	public static String DIR_DATA = "data";
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String listURL = "" ;
-		for(int page=2;page<15;page++){
-			try {
-				listURL = URL_ROOT1 + page + URL_ROOT2;
-//				System.out.println("parsing.. " + listURL);
-				Document docRoot = Jsoup.connect(listURL).userAgent("Mozilla").timeout(5000).get();
-//				System.out.println(docRoot);
-				for(Element ele : docRoot.select(".tile-grid-unit-wrapper")){
+    public static String URL_ROOT1 = "http://www.walmart.com/browse/video-games/playstation-3-accessories/2636_413799_1225016?page=";
+    public static String URL_ROOT2 = "&cat_id=2636_413799_1225016";
+    public static String DIR_DATA = "data";
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String listURL = "";
+        for (int page = 2; page < 15; page++) {
+            try {
+                listURL = URL_ROOT1 + page + URL_ROOT2;
+                Document docRoot = Jsoup.connect(listURL).userAgent("Mozilla").timeout(5000).get();
+                //$("#searchProductResult ul")
+                for (Element ele : docRoot.select(".tile-grid-unit-wrapper")) {
 //					System.out.println(ele);
-					WalmartProduct product = new WalmartProduct();
-					
-				    String detailUrl = ele.select(".js-product-image").first().attr("href");
-				    try{
-				    	Document detailPage = Jsoup.connect("http://www.walmart.com"+detailUrl).userAgent("Mozilla").timeout(5000).get();
-				    	Element name = detailPage.select(".product-name").first();
-				    	product.setProductName(name.text());
-				    	
-				    	Element code = detailPage.select(".product-subhead-walmartnumber").first();
-				    	if(code!=null){
-				    		product.setProductCode(code.text());
-				    	}
-				    	
-				    	Element price = detailPage.select(".js-price-display").first();
-				    	product.setProductPrice(price.text());
-				    	
-				    	
-				    }catch(IOException e){
-				    	System.err.println("Error: " + e.getMessage());
-				    }
-				    System.out.println(product);
-				    Thread.sleep(1000);
-				}
-			} catch (Exception e) { System.err.println("Error: " + e.getMessage()); } 
-			
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		}
+                    WalmartProduct product = new WalmartProduct();
+
+                    String detailUrl = ele.select(".js-product-image").first().attr("href");
+                    try {
+                        Document detailPage = Jsoup.connect("http://www.walmart.com" + detailUrl).userAgent("Mozilla").timeout(5000).get();
+                        Element name = detailPage.select(".product-name").first();
+                        product.setProductName(name.text());
+
+                        Element code = detailPage.select(".product-subhead-walmartnumber").first();
+                        if (code != null) {
+                            product.setProductCode(code.text());
+                        }
+
+                        Element price = detailPage.select(".js-price-display").first();
+                        product.setProductPrice(price.text());
+
+
+                    } catch (IOException e) {
+                        System.err.println("Error: " + e.getMessage());
+                    }
+                    System.out.println(product);
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
 
 }
